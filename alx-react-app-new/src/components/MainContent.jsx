0311e6669f0bb2fada6c98fb72b9c2194d501221
Desuwaa <airekpita@gmail.com>
2025-07-19 22:00:31 +0100

    basic styling to the react files

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main>
+      <p style={{ color: 'orange' }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
