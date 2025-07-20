62403b5dff7d444203c2a32b7ac29ce35dfebc7c
Desuwaa <airekpita@gmail.com>
2025-07-20 08:18:27 +0100

    simple counter app

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,24 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import WelcomeMessage from "./components/WelcomeMessage";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-import UserProfile from "./components/UserProfile";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <WelcomeMessage />
-      <Header />
-      <MainContent />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <Footer />
-    </>
+    <div>
+      <h1>Simple Counter App</h1>
+      <Counter />
+    </div>
   );
 }
 
