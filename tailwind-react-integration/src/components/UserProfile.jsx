ebe2551f9cc2908d9de790cc3c1f5789eb58a6c7
Olaitan Adedokun <kenzdex@gmail.com>
2025-08-09 09:19:30 +0100

    adjust while on smaller

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -16,7 +16,7 @@ function UserProfile() {
       >
         John Doe
       </h1>
-      <p className="text-gray-600 text-base">
+      <p className="text-gray-600 text-base sm:text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
